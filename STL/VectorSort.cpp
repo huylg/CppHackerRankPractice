42c18aea4b4e6ded1f9424dc9e5921cfb787d9a8
huylg <huylg@vng.com.vn>
2020-03-28 00:09:27 +0700

    more practice

---
+++ STL/VectorSort.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+    vector<int> vec;
+    vec.resize(n);
+    for(int i=0;i<n;i++) cin>>vec[i];
+    sort(vec.begin(),vec.end());
+    for(int i=0;i<n;i++) cout<<vec[i]<<" ";
+    return 0;
+}
+
